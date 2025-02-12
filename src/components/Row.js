9c14479e75f5758ddc7aa5b29e1f05ac6700fdb7
Danylo Baziewicz <danxbazi@gmail.com>
2025-02-12 01:18:29 -0300

    criação de arquivo para consumir API e retornar as informações necessárias para exibir em tela.

---
+++ src/components/Row.js
@@ -0,0 +1,37 @@
+import React, { useEffect } from 'react'
+import { getMovies } from '../api';
+
+const imageHost = "http://image.tmdb.org/t/p/original/"
+function Row({title, path}) {
+    const [movies, setMovies] = React.useState([]);
+    
+    const fetchMovies = async (_path) => {
+        try {
+            const data = await getMovies(_path)
+            console.log('data', data);
+            setMovies(data?.results);
+        } catch(error) {
+            console.log("fetchMovies error:", error);
+        }
+    };
+
+    useEffect(() => {
+      fetchMovies(path)
+    }, [path]);
+    
+
+  return (
+    <div className='row-container'>
+        <h2 className='row-header'>{title}</h2>
+        <h2 className='row-cards'>
+            {movies?.map(movie => {
+                return (
+                    <img key={movie.id} src={`${imageHost}${movie.poster_path}`} alt={movie.name}></img>
+                )
+            })}
+        </h2>
+    </div>
+  )
+}
+
+export default Row;
