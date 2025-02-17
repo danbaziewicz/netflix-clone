ea075713f1a14ad43dee9d07f23ab55b586d7a28
Danylo Baziewicz <danxbazi@gmail.com>
2025-02-17 19:17:08 -0300

    adicionado estilos e nav, banner e exibição de categorias.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import categorias from './api';
 import './App.css';
+import Banner from './components/Banner';
+import Nav from './components/Nav';
 import Row from './components/Row';
 
 function App() {
   return (
     <div className='App'>
-      {/*Navbar*/}
-      {/*destaque*/}
-      {/*em alta*/}
+      <Nav/>
+      <Banner />
       {categorias.map((category) => {
         return (
           <Row key={category.name} 
           title={category.title} 
           path={category.path}
+          isLarge={category.isLarge}
           />
         );
         })}
