8811a57be4c7a75ffdd1d31995144c97b028edb9
Danylo Baziewicz <danxbazi@gmail.com>
2025-02-17 19:16:09 -0300

    adicionado api_key e alterações em api.js para imagens grandes para banner.

--- src/api.js
+++ src/api.js
@@ -1,35 +1,41 @@
-const API_KEY = "917e48a57cf078c2f45fbb6442d09ace";
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 const categorias = [
     {
         name: "trending",
         title: "Em alta",
-        path: `/trending/all/week?language=en-US&api_key=${API_KEY}`
+        path: `/trending/all/week?language=en-US&api_key=${API_KEY}`,
+        isLarge: true,
     },
     {
-        name: "netflixOriginais",
+        name: "netflixOriginals",
         title: "Originais Netflix",
-        path: `/discover/tv?with_network=213&api_key=${API_KEY}`
+        path: `/discover/tv?with_network=213&api_key=${API_KEY}`,
+        isLarge: false,
     },
     {
         name: "topRated",
         title: "Populares",
-        path: `/movie/top_rated?language=en-US&api_key=${API_KEY}`
+        path: `/movie/top_rated?language=en-US&api_key=${API_KEY}`,
+        isLarge: false,
     },
     {
         name: "comedy",
         title: "Comédia",
-        path: `/discover/tv?with_genres=35&api_key=${API_KEY}`
+        path: `/discover/tv?with_genres=35&api_key=${API_KEY}`,
+        isLarge: false,
     },
     {
         name: "romances",
         title: "Romances",
-        path: `/discover/tv?with_genres=10749&api_key=${API_KEY}`
+        path: `/discover/tv?with_genres=10749&api_key=${API_KEY}`,
+        isLarge: false,
     },
     {
         name: "documentaries",
         title: "Documentários",
-        path: `/discover/tv?with_genres=99&api_key=${API_KEY}`
+        path: `/discover/tv?with_genres=99&api_key=${API_KEY}`,
+        isLarge: false,
     },
 ];
 
